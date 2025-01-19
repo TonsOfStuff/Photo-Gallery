61b77295b3caf2e7bb9d320e032005d2afaf3e70
TonsOfStuff <jtons2008@gmail.com>
2025-01-19 14:35:29 -0600

    you can now go thru the images

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const backward = document.getElementById("backward");
 
 const displayer = document.getElementById("displayer");
 
+const imageList = [
+    "scr00000.jpg",
+    "scr00001.jpg",
+    "scr00002.jpg"
+]
+
+let count = 0;
+
 
 function goForward() {
-    displayer.src = "photos/scr00001.jpg"
+    count += 1;
+    if (count > imageList.length - 1){
+        count = 0;
+    }
+    displayer.src = "photos/" + imageList[count]
+}
+
+function goBackward() {
+    count -= 1;
+    if (count < 0){
+        count = imageList.length - 1;
+    }
+    displayer.src = "photos/" + imageList[count]
 }
