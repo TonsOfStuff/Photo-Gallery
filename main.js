0c0ffa50496a9ea85d6591adc9b491411f91e4e0
TonsOfStuff <jtons2008@gmail.com>
2025-01-19 14:21:40 -0600

    photo gallery app stuff

---
+++ main.js
@@ -0,0 +1,8 @@
+const forward = document.getElementById("forward");
+const backward = document.getElementById("backward");
+
+const displayer = document.getElementById("displayer");
+
+function goForward(){
+    displayer.src = "photos/scr00001.jpg"
+}
