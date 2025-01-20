9bed1d5e97adb05dd4bf7e37e7e4f1eca8e3609c
TonsOfStuff <jtons2008@gmail.com>
2025-01-19 21:14:05 -0600

    sliding animation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const forward = document.getElementById("forward");
 const backward = document.getElementById("backward");
 
 const displayer = document.getElementById("displayer");
+const displayer2 = document.getElementById("displayer2");
 
 
 //Construction of classes
@@ -22,17 +23,46 @@ const imageList = [
 let count = 0;
 
 function goForward() {
+    forward.disabled = true;
     count += 1;
     if (count > imageList.length - 1){
         count = 0;
     }
-    displayer.src = "photos/" + imageList[count].id
+    displayer2.src = "photos/" + imageList[count].id
+    displayer2.style.display = "block";
+    displayer2.style.position = "absolute";
+
+    displayer.style.animation = "imageMove 2s forwards";
+    displayer2.style.animation = "imageMove2 2s forwards"; 
+    setTimeout(() => {
+        displayer.style.animation = "none";
+        displayer.src = displayer2.src;
+
+        displayer2.style.animation = "none";
+
+        forward.disabled = false;
+    }, 2000) 
+    
+
 }
 
 function goBackward() {
+    backward.disabled = true;
     count -= 1;
     if (count < 0){
         count = imageList.length - 1;
     }
+    displayer2.src = displayer.src;
     displayer.src = "photos/" + imageList[count].id
+    displayer2.style.display = "block";
+    displayer2.style.position = "absolute";
+
+    displayer.style.animation = "imageMove 2s forwards reverse";
+    displayer2.style.animation = "imageMove2 2s forwards reverse"; 
+    setTimeout(() => {
+        displayer.style.animation = "none";
+        displayer2.style.animation = "none";
+
+        backward.disabled = false;
+    }, 2000) 
 }
