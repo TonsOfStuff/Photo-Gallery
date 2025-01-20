69877d00411cf7435c83d33233768539a2640978
TonsOfStuff <jtons2008@gmail.com>
2025-01-19 20:26:55 -0600

    classes

--- main.js
+++ main.js
@@ -3,21 +3,30 @@ const backward = document.getElementById("backward");
 
 const displayer = document.getElementById("displayer");
 
+
+//Construction of classes
+class ImageDisplay{
+    constructor(id, tags, info) {
+        this.id = id;
+        this.tags = tags;
+        this.info = info;
+    }
+}
+
 const imageList = [
-    "scr00000.jpg",
-    "scr00001.jpg",
-    "scr00002.jpg"
+    new ImageDisplay("scr00000.jpg", ["Space"], "Picture of space"),
+    new ImageDisplay("scr00001.jpg", ["Space"], "Picture of space"),
+    new ImageDisplay("scr00002.jpg", ["Space"], "Picture of space")
 ]
 
 let count = 0;
-
 
 function goForward() {
     count += 1;
     if (count > imageList.length - 1){
         count = 0;
     }
-    displayer.src = "photos/" + imageList[count]
+    displayer.src = "photos/" + imageList[count].id
 }
 
 function goBackward() {
@@ -25,5 +34,5 @@ function goBackward() {
     if (count < 0){
         count = imageList.length - 1;
     }
-    displayer.src = "photos/" + imageList[count]
+    displayer.src = "photos/" + imageList[count].id
 }
